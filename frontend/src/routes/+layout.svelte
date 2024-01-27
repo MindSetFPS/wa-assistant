328f0aaf95f977c49d9bd93ad904d725023dba81
MindSetFPS <45702604+MindSetFPS@users.noreply.github.com>
2024-01-26 23:03:38 -0600

    Update +layout.svelte

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,28 @@
 <script>
-	import '../app.pcss';
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+    import "../app.pcss";
+  import { page } from '$app/stores';
+  import { DarkMode } from 'flowbite-svelte';
+
+    $: activeUrl = $page.url.pathname;
 </script>
 
-<slot />
+<Navbar>
+    <NavBrand href="/">
+      <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
+      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+    </NavBrand>
+    <NavHamburger />
+    <NavUl {activeUrl}>
+      <NavLi href="/">Home</NavLi>
+      <NavLi href="/setup">Setup</NavLi>
+      <NavLi href="/chat">chat</NavLi>
+      <NavLi href="/docs/components/alert">Alert</NavLi>
+      <NavLi href="/docs/components/avatar">Avatar</NavLi>
+      <NavLi> <DarkMode /> </NavLi>
+    </NavUl>
+  </Navbar>
+
+<div class="container mx-auto md:px-36">
+    <slot></slot>
+</div>
