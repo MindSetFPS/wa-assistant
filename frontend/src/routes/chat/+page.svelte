0a6d42531da629be2423e21f98a940ef51ef23eb
Daniel Escobar <daanielescobar@gmail.com>
2024-01-28 17:05:15 -0600

    barebones chat page

--- frontend/src/routes/chat/+page.svelte
+++ frontend/src/routes/chat/+page.svelte
@@ -1,16 +1,22 @@
 <script>
-    import { Button } from "flowbite-svelte";
-    
+    let lang = 'ts'
+    import { Textarea, Alert, ToolbarButton, P } from 'flowbite-svelte';
+    import { ImageOutline, FaceGrinOutline, PapperPlaneOutline, BarsSolid, ChevronUpSolid } from 'flowbite-svelte-icons';
+    import { Button, Dropdown, DropdownItem } from 'flowbite-svelte';
+
+    import Chat from '../../components/chat.svelte';
+	import SideBar from '../../components/sideBar.svelte';
+
+    let placement = 'top'
     let text = "";
-    let a = 1;
-    let b = 5;
-    let total = 0;
+    let prompt = "";
+    let chatHistory = [];
     function askApi() {
         fetch("http://192.168.1.140:7861/prompt", {
             method: "POST",
             body: JSON.stringify({
                 max_tokens: 128,
-                prompt: 'hola',
+                prompt: prompt,
                 from: '999999999',
             }),
             headers: {
@@ -19,31 +25,61 @@
             },
         })
             .then((response) => response.json())
-            .then((data) => (answer = data.answer))
+            .then((data) => ( chatHistory = [data.answer, ...chatHistory]))
             .catch((error) => {
                 //handle error
             });
     }
 
-	async function add() {
-		const response = await fetch('/api/add', {
-			method: 'POST',
-			body: JSON.stringify({ a, b }),
-			headers: {
-				'content-type': 'application/json'
-			}
-		});
+    function addMessage(){
+        askApi(prompt)
+        chatHistory = [prompt, ...chatHistory]
+        // chatHistory = chatHistory.reverse()
+        console.log(chatHistory)
+        prompt = "";
+    }
 
-		total = await response.json();
-	}
+    function keypress(e){
+        if(e.key == "Enter"){
+            console.log(e.key)
+            addMessage()
+            prompt = "";
+        }
+    }
 </script>
 
-<input type="text" name="prompt" id="prompt" placeholder="Escribe aqui.." />
-<!-- <input type="number" bind:value={a}> +
-<input type="number" bind:value={b}> =
-{total} -->
-
-<!-- <button on:click={add}>Calculate</button> -->
-<Button on:click={() => askApi()}>
-    Ask AI
-</Button>
+<!-- <SideBar /> -->
+<div class="h-full flex flex-col justify-between">
+    <Chat {chatHistory} />
+    <form class="w-full" >
+        <label for="chat" class="sr-only">Your message</label>
+        <Dropdown {placement} triggeredBy="#dropdown">
+            <DropdownItem>Dashboard</DropdownItem>
+            <DropdownItem>Settings</DropdownItem>
+            <DropdownItem>Earnings</DropdownItem>
+            <DropdownItem slot="footer">Sign out</DropdownItem>
+        </Dropdown>
+        <Alert color="dark" class="px-1 py-2">
+            <svelte:fragment slot="icon">
+                <ToolbarButton color="dark" class="text-gray-500 dark:text-gray-400" id="dropdown">
+                    <BarsSolid class="w-5 h-5" />
+                    <span class="sr-only">Upload image</span>
+                </ToolbarButton>
+                <Textarea 
+                    id="chat" 
+                    class="mx-1" 
+                    rows="1" 
+                    placeholder="Your message..." 
+                    bind:value={prompt} 
+                    on:keypress={(e) => keypress(e)}
+                />
+                <ToolbarButton type="submit" color="blue" class="rounded-full text-primary-600 dark:text-primary-500"
+                on:click={() => addMessage()}
+                >
+                <PapperPlaneOutline class="w-5 h-5 rotate-45" />
+                <span class="sr-only">Send message</span>
+            </ToolbarButton>
+        </svelte:fragment>
+    </Alert>
+</form>
+</div>
