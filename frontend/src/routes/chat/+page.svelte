c4a1def2de067f912033c07d94207fce21d46771
MindSetFPS <45702604+MindSetFPS@users.noreply.github.com>
2024-01-31 15:32:06 -0600

    Create deleteConversation function and create types

--- frontend/src/routes/chat/+page.svelte
+++ frontend/src/routes/chat/+page.svelte
@@ -4,34 +4,50 @@
 	import ChatTextInput from '../../components/chatTextInput.svelte';
 	import ChatTopBar from '../../components/chatTopBar.svelte';
 
-	let text = '';
-	let prompt = '';
+	type owner = "Bot" | string;
+	interface Message {
+		owner: owner,
+		text: string
+	}
+	
 	let chatHistory: string[] = [];
 	let hidden = false;
+	let user: owner = "9999071819"
 	
 	function handleNewMessage(a){
 		console.log(a)
 		addMessage(a.detail.prompt)
 	}
-
-	function handleDeleteChat(){
-		chatHistory = [];
-	}
-
+	
+	const API_URL: string = 'http://192.168.1.140:7861/';
 	function addMessage(message: string) {
 		askApi(message);
 		chatHistory = [message.trim(), ...chatHistory];
 		console.log(chatHistory);
 	}
 
+	function handleDeleteChat(){
+		chatHistory = [];
+		fetch(API_URL + 'delete-conversation', {
+			method: 'POST',
+			body: JSON.stringify({
+				from: user
+			}),
+			headers: {
+				'Content-type': 'application/json; charset=UTF-8',
+				'Access-Control-Allow-Origin': '*'
+			}
+		})
+	}
+
 	function askApi(message: string) {
 		console.log(message);
-		fetch('http://192.168.1.140:7861/prompt', {
+		fetch(API_URL + 'prompt', {
 			method: 'POST',
 			body: JSON.stringify({
 				max_tokens: 64,
 				prompt: message,
-				from: '999999999'
+				from: user
 			}),
 			headers: {
 				'Content-type': 'application/json; charset=UTF-8',
